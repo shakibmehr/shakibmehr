9bdc930be12f30db13d586a594a9d4bb47370dfd
shakibmehr <ali_shakibmehr@yahoo.com>
2020-03-19 14:09:45 +0330

    Add files via upload

--- qmybitmap.cpp
+++ qmybitmap.cpp
@@ -2,5 +2,29 @@
 
 QMyBitMap::QMyBitMap(QObject *parent) : QObject(parent)
 {
+    this->current_image = QImage(":/1.png");
+}
 
+void QMyBitMap::setImage(const QImage &image)
+{
+    this->current_image = image;
+   // update();
+}
+
+void QMyBitMap::paint(QPainter *painter)
+{
+    QRectF bounding_rect ;
+    QImage scaled = this->current_image.scaledToHeight(bounding_rect.height());
+    QPointF center = bounding_rect.center() - scaled.rect().center();
+
+    if(center.x() < 0)
+        center.setX(0);
+    if(center.y() < 0)
+        center.setY(0);
+   painter->drawImage(center, scaled);
+}
+
+QImage QMyBitMap::image() const
+{
+    return this->current_image;
 }
--- qmybitmap.h
+++ qmybitmap.h
@@ -2,16 +2,28 @@
 #define QMYBITMAP_H
 
 #include <QObject>
+#include <QQuickPaintedItem>
+#include <QQuickItem>
+#include <QPainter>
+#include <QImage>
 
 class QMyBitMap : public QObject
 {
     Q_OBJECT
+        Q_PROPERTY(QImage image READ image WRITE setImage NOTIFY imageChanged)
 public:
     explicit QMyBitMap(QObject *parent = nullptr);
+    Q_INVOKABLE void setImage(const QImage &image);
+    void paint(QPainter *painter);
+    QImage image() const;
 
 signals:
+void imageChanged();
 
 public slots:
+
+private:
+    QImage current_image;
 };
 
 #endif // QMYBITMAP_H
